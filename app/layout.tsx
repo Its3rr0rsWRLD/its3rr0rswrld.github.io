7d50b232fb43732f974e9f4913218a81ccb5fb47
3rr0r <92172873+Its3rr0rsWRLD@users.noreply.github.com>
2025-03-05 23:11:45 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
